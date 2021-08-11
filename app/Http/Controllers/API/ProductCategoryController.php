762809706fa43c9a43a47eb38f9bc7c8f07a197d
Mada Jabbar <madajabbar22@gmail.com>
2021-08-11 21:05:15 +0700

    first commit

---
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    public function all(Request $request){
+        $id = $request->input('id');
+        $name = $request->input('name');
+        $limit = $request->input('limit');
+        $show_product = $request->input('show_product');
+
+        if($id){
+            $category = ProductCategory::with(['products'])->find($id);
+
+            if($category){
+                return ResponseFormatter::success(
+                    $category,
+                    'Data kategori berhasil diambil'
+                );
+            }
+            else{
+                return ResponseFormatter::success(
+                    null,
+                    'Data tidak ada',
+                    404
+                );
+            }
+        }
+        $category = ProductCategory::query();
+
+        if($name) {
+            $category->where('name','Like','%'.$name.'%');
+        }
+        if($show_product){
+            $category->with('products');
+        }
+
+        return ResponseFormatter::success(
+            $category->paginate($limit),
+            'Data kategori berhasil diambil'
+        );
+    }
+}
